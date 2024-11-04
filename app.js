d343776999f3c7df46d0005b70d2bbf31b09f0a6
devaziz1 <157882617+devaziz1@users.noreply.github.com>
2024-11-04 16:34:10 +0500

    device tracked successful

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user-disconnect", socket.id);
+  });
+});
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(8080, () => {
+  console.log("Server running");
+});
